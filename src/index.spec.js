52636d235955e38e61ca6c0cdb3259bec98896cf
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-10 14:43:57 -0500

    Move mock data to a function so it can be included at the end of the test file

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,41 +1,47 @@
 import { get, getFile } from './index.js'
 
-const mockData = [
-    {
-        id: 1,
-        city: 'College Station',
-        state: 'TX',
-        year: 2023,
-        visited: true,
-        resided: true,
-        population: 122807,
-        area: 49.6,
-        latitude: 30.628,
-        longitude: -96.334,
-        rating: 1.0,
-    },
-    {
-        id: 2,
-        city: 'Austin',
-        state: 'TX',
-        year: 2023,
-        visited: true,
-        population: 978908,
-        area: 322.48,
-        latitude: 30.267,
-        longitude: -97.743,
-        rating: null,
-    },
-]
-
 describe('get', () => {
     it('should return an object', () => {
-        expect(typeof get(mockData)).toBe('object')
+        const schema = get(mockData())
+        expect(typeof schema).toBe('object')
+        expect(schema == null).toBe(false)
     })
 })
 
 describe('getFileContents', () => {
     it('should return a string', () => {
-        expect(typeof getFile(mockData)).toBe('string')
+        const schemaFile = getFile(mockData())
+        expect(typeof schemaFile).toBe('string')
+        expect(schemaFile).not.toBeFalsy()
     })
 })
+
+function mockData() {
+    return [
+        {
+            id: 1,
+            city: 'College Station',
+            state: 'TX',
+            year: 2023,
+            visited: true,
+            resided: true,
+            population: 122807,
+            area: 49.6,
+            latitude: 30.628,
+            longitude: -96.334,
+            rating: 1.0,
+        },
+        {
+            id: 2,
+            city: 'Austin',
+            state: 'TX',
+            year: 2023,
+            visited: true,
+            population: 978908,
+            area: 322.48,
+            latitude: 30.267,
+            longitude: -97.743,
+            rating: null,
+        },
+    ]
+}
