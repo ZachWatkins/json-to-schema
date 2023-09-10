50aa57e5ebce64953fcf0e58352655864c5856c2
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-10 15:06:21 -0500

    Add test for default schema object generation

--- src/index.spec.js
+++ src/index.spec.js
@@ -6,6 +6,36 @@ describe('get', () => {
         expect(typeof schema).toBe('object')
         expect(schema == null).toBe(false)
     })
+    it('should return a schema object', () => {
+        const schema = get(mockData())
+        expect(Object.keys(schema)).toEqual([
+            'id',
+            'city',
+            'state',
+            'year',
+            'visited',
+            'resided',
+            'population',
+            'area',
+            'latitude',
+            'longitude',
+            'rating',
+        ])
+        // Confirm the default schema output.
+        expect(schema).toEqual({
+            id: { type: 'number', required: true },
+            city: { type: 'string', required: true },
+            state: { type: 'string', required: true },
+            year: { type: 'number', required: true },
+            visited: { type: 'boolean', required: true },
+            resided: { type: 'boolean', required: false },
+            population: { type: 'number', required: true },
+            area: { type: 'number', required: true },
+            latitude: { type: 'number', required: true },
+            longitude: { type: 'number', required: true },
+            rating: { type: 'number', required: true, allowNull: true },
+        })
+    })
 })
 
 describe('getFileContents', () => {
