2686f9e373c6a4b90d88a7de61f24c59b5ea6786
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-10 19:34:49 -0500

    Move class file logic to modules

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
  * @year 2023
  */
 
+import Schema from './schema'
+
 /**
  * Get a schema object inferred from an array of flat objects.
  * @param {object[]|[]} arrayOfObjects - Array of objects to evaluate.
@@ -12,61 +14,6 @@
  * @returns {Schema} Returns a schema object.
  */
 export function get(arrayOfObjects, pluginOptions = {}) {
-    console.log(
-        arrayOfObjects.length,
-        'rows found, getting schema file contents for options:',
-        pluginOptions
-    )
-
-    function SchemaPlugin(options) {
-        this.options = options
-        this.typeOfValue = function (value) {
-            return typeof value !== 'object' || value !== null
-                ? typeof value
-                : 'null'
-        }
-    }
-
-    function Schema(arrayOfObjects, pluginOptions) {
-        const types = new FoundTypes(
-            arrayOfObjects,
-            new SchemaPlugin(pluginOptions)
-        )
-        for (const prop in types.byProperty) {
-            const typeList = Object.keys(types.byProperty[prop])
-            const typeValues = typeList.filter(
-                (type) => type !== 'null' && type !== 'undefined'
-            )
-            this[prop] = {
-                type: typeValues.length === 1 ? typeValues[0] : typeValues,
-                required: typeList.indexOf('undefined') === -1,
-            }
-            if (typeList.indexOf('null') !== -1) {
-                this[prop].nullable = true
-            }
-        }
-    }
-
-    function FoundTypes(arrayOfObjects, plugin) {
-        this.properties = arrayOfObjects
-            .map((row) => Object.keys(row))
-            .flat()
-            .filter((value, index, self) => self.indexOf(value) === index)
-        this.byProperty = arrayOfObjects.reduce((types, row) => {
-            for (let i = 0; i < this.properties.length; i++) {
-                const prop = this.properties[i]
-                if (types[prop] === undefined) {
-                    types[prop] = {}
-                }
-                let schemaType = plugin.typeOfValue(row[prop])
-                if (types[prop][schemaType] === undefined) {
-                    types[prop][schemaType] = true
-                }
-            }
-            return types
-        }, {})
-    }
-
     return new Schema(arrayOfObjects, pluginOptions)
 }
 
